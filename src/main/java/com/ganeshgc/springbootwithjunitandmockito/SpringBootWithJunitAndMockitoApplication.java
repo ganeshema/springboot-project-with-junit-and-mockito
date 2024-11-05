07e97d66651bd91313f006924d13359b0c2cc214
ganeshema <ganeshlema2021@gmail.com>
2024-11-04 21:18:04 -0600

    initial commit

---
+++ src/main/java/com/ganeshgc/springbootwithjunitandmockito/SpringBootWithJunitAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.ganeshgc.springbootwithjunitandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJunitAndMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithJunitAndMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ganeshgc/springbootwithjunitandmockito/SpringBootWithJunitAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshgc.springbootwithjunitandmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJunitAndMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
